ea578ed5320501dc750ad5014a943aa59aa6a0b0
Liam McFadden <liamm18@vt.edu>
2022-07-21 23:28:28 -0400

    added argument support and help handler

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[$# < 3]] echo 
+# Display help
+help() {
+    echo
+    echo "See https://wiki.osdev.org/GCC_Cross-Compiler for details on what prefix, path, and target are."
+    echo
+    echo "Usage:  build.sh <prefix> <path> <target>"
+    echo
+} 
+
+# Check for help option
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+    help
+    exit
+fi
+
+# Set target, path, and prefix.
+if [[ $# != 3 ]]; then
+    help
+else
+    prefix=$1
+    path=$2
+    target=$3
+fi
